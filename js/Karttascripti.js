b72fc1989e545bcf433e17d6062f15cd10f12457
Tino <tino.paajanen@hotmail.fi>
2021-04-27 11:30:10 +0300

    Kartta säätöä

--- js/Karttascripti.js
+++ js/Karttascripti.js
@@ -1,6 +1,10 @@
 'use strict'
-const map = L.map('map').setView([60, 25], 5);
-map.flyTo([60.2238, 24.758], 17)
+const map = L.map('map').setView([60.2238, 24.758], 17);
+
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 }).addTo(map);
+
+L.marker([60.2238, 24.758]).addTo(map)
+  .bindPopup("<a href='https://www.metropolia.fi/fi/metropoliasta/kampukset/karamalmi' target='_blank'>Metropolia Karamalmin kampus</a>")
+  .openPopup();
